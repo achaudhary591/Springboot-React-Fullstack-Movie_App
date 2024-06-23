15090cafdfe9f0afff53811d0a3010b4939c82f5
achaudhary591 <achaudhary591@gmail.com>
2024-06-23 14:20:53 +0530

    Fullstack React + Springboot using MongoDb movie list app for learning

---
+++ Backend/movieist/src/main/java/dev/akshay/movieist/MovieistApplication.java
@@ -0,0 +1,30 @@
+package dev.akshay.movieist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "dev.akshay.movieist.movies")
+public class MovieistApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MovieistApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+}
